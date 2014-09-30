70fdb66dc7a0966c31039f5bc71017372cfc4a4f
martinorocco@gmail.com <martinorocco@gmail.com@edca3a55-eeb9-cfb6-f6e5-bb303a670d00>
2014-09-30 19:40:00 +0000

    tagged 1.2.2

---
+++ src/osgWrappers/serializers/osgODEUtil/LibraryWrapper.cpp
@@ -0,0 +1,11 @@
+#include <osgDB/Registry>
+
+USE_SERIALIZER_WRAPPER(AnimationHelper) ;
+USE_SERIALIZER_WRAPPER(AnimationManager) ;
+USE_SERIALIZER_WRAPPER(HashTable) ;
+USE_SERIALIZER_WRAPPER(HashTablePair) ;
+USE_SERIALIZER_WRAPPER(Picker) ;
+
+extern "C" void wrapper_serializer_library_osgodeutil(void) {}
+
+ 
